773b0c3e9f54066714f33e4366eb647bf14f7874
sonvu056 <sonvu056@gmail.com>
2020-11-30 18:42:55 +0700

    1

---
+++ Buoi 14/js.js
@@ -0,0 +1,29 @@
+// let a = [1, 2, 3, 4, 5];
+
+// function sum(a) {
+//     let s = 0;
+//     for (i = 0; i < a.length; i++) {
+//         s += a[i];
+//     }
+//     return s
+// }
+// console.log(sum(a));
+
+// function tbc(a) {
+//     let s = 0;
+//     for (i = 0; i < a.length; i++) {
+//         s += a[i];
+//     }
+//     return (s / 2);
+// }
+// console.log(tbc(a));
+
+// function index(a, n) {
+//     for (i = 0; i < a.length; i++) {
+//         if (a[i] == n) {
+//             return i
+//         }
+//     } 
+// }
+
+// console.log(index(a, 2));
