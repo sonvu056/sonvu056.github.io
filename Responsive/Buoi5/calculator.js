3cef2b0ad8860635993e5a6c2dc75db423cd1e98
sonvu056 <sonvu056@gmail.com>
2020-12-11 20:49:42 +0700

    1

--- Responsive/Buoi5/calculator.js
+++ Responsive/Buoi5/calculator.js
@@ -1,12 +1,24 @@
 temp = "";
+flag = 0;
+function addFlag(val) {
+  flag = val;
+}
 function clr() {
   document.getElementById("line1").innerText = "";
   document.getElementById("line2").innerText = "0";
   temp = "";
 }
 function dis(val) {
-  document.getElementById("line1").innerText += val;
   temp = document.getElementById("line1").innerText;
+  if (
+    temp[temp.length] != "×" ||
+    temp[temp.length] != "÷" ||
+    temp[temp.length] != "+" ||
+    temp[temp.length] != "-"
+  ) {
+    document.getElementById("line1").innerText += val;
+    temp = document.getElementById("line1").innerText;
+  }
   document.getElementById("line2").innerText = "";
 }
 function solve() {
@@ -23,3 +35,4 @@ function minus() {
   }
   document.getElementById("line2").innerText = temp;
 }
+function perc() {}
