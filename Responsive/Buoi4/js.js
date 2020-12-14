5fdab7cb8dc48cceb6fa105a8498229cce5a564a
sonvu056 <sonvu056@gmail.com>
2020-12-14 11:03:59 +0700

    1

--- Responsive/Buoi4/js.js
+++ Responsive/Buoi4/js.js
@@ -15,9 +15,21 @@ function copyContent(paragraph1, paragraph2) {
 }
 
 function changeFontSize(x) {
-    document.getElementById("main").style.fontSize = x + "px";
+    document.getElementById("p1").style.fontSize = x + "px";
+    document.getElementById("p2").style.fontSize = x + "px";
+    document.getElementById("p3").style.fontSize = x + "px";
 }
 
-// function increaseFontSize(paragraph) {
+function increaseFontSize(paragraph) {
+    let fontSize = document.getElementById(paragraph).style.fontSize;
+    if (+fontSize.slice(0, -2) < 30) {
+        document.getElementById(paragraph).style.fontSize = +fontSize.slice(0,-2) + 1 + "px";  
+    } 
+}
 
-// }
+function decreaseFontSize(paragraph) {
+    let fontSize = document.getElementById(paragraph).style.fontSize;
+    if (+fontSize.slice(0, -2) > 10) {
+       document.getElementById(paragraph).style.fontSize = +fontSize.slice(0, -2) - 1 + "px";
+    } 
+}
